2869c3918f1a6792c5c9e5a9ae74b044908f760d
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2023-06-28 16:17:15 -0300

    sinc
    Não está salvando os dados no banco é preciso arrumar.

--- loja/__init__.py
+++ loja/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 # create the extension
@@ -8,8 +9,10 @@ db = SQLAlchemy()
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///minhaloja.db"
+app.config['SECRET_KEY']='123456'
 # initialize the app with the extension
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 
 from loja.admin import rotas
--- loja/admin/rotas.py
+++ loja/admin/rotas.py
@@ -1,9 +1,11 @@
 from contextlib import _RedirectStream
-
-
-from flask import render_template, session, request, url_for,flash
-from loja import app, db
+from flask import render_template, session, request, redirect, url_for, flash
+from loja import app, db, bcrypt
 from loja.admin.formulario import RegistrationForm
+from .models import User
+import os
+
+from loja.admin.models import User
 
 
 @app.route('/')
@@ -15,9 +17,12 @@ def home():
 def registrar():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        # form.password.data)
-        # db_session.add(user)
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data,
+                    username=form.username.data,
+                    email=form.email.data,
+                    password=hash_password)
+        db.session.add(user)
         flash('Obrigado por se registrar.')
         return _RedirectStream(url_for('login'))
     return render_template('admin/registrar.html', form=form, title="Pagina de registros")
